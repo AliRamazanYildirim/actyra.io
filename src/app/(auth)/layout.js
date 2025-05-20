e39d806000222aa2aba7c7bf9a35e31584acc461
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2025-05-20 10:05:19 +0200

    refact: entferne InitUser-Komponente aus AuthLayout und lösche die Datei

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,4 +1,3 @@
-import InitUser from '@/components/InitUser';
 import Stars from '@/components/Stars';
 import React from 'react';
 
@@ -6,7 +5,6 @@ const AuthLayout = ({ children }) => {
   return (
     <div className="min-h-screen  dark:bg-[#0d0e25] flex items-center justify-center">
       <Stars/>
-      <InitUser/>
       {children}
     </div>
   );
